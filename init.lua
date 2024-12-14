1076f867e49306e3f2ff004d90345b43aad35559
tkykd <taogaoshen@outlook.com>
2024-12-14 13:50:31 +0800

    add formatter for language html

--- init.lua
+++ init.lua
@@ -748,6 +748,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
+        html = { 'prettier' },
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
